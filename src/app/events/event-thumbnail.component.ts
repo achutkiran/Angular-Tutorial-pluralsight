7809aa71f0d67b3877d6b0135930bf00460528cd
Achut Cherukuri <Achut.Cherukuri@primeroedge.com>
2019-01-25 16:01:12 -0600

    adding styles

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,7 +2,10 @@ import { Component, Input, EventEmitter, Output } from '@angular/core'
 
 @Component({
     selector: 'event-thumbnail',
-    templateUrl:'./event-thumbnail.component.html'
+    templateUrl:'./event-thumbnail.component.html',
+    styles: [`
+        .pad-left { margin-left:10px; }
+    `]
 })
 
 export class EventThumbnailComponent {
