bbbf64fa4b25d759ba69504c162c58016492fe29
Achut Cherukuri <Achut.Cherukuri@primeroedge.com>
2019-01-25 15:56:37 -0600

    call child functions or fetch child values from parent using template varibales

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,4 +7,9 @@ import { Component, Input, EventEmitter, Output } from '@angular/core'
 
 export class EventThumbnailComponent {
     @Input() event: any
+    childValue:string ="some value"
+
+    logFoo(){
+        console.log('foo') 
+     }
 }
