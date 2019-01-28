a1940aacd3acfc3ebb000673fe482ce069a3dab3
Achut Cherukuri <Achut.Cherukuri@primeroedge.com>
2019-01-28 17:12:25 -0600

    reverting back to one  way data binding

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Router } from '@angular/router';
 import { EventService } from './shared/event.service';
 
@@ -13,26 +13,10 @@ import { EventService } from './shared/event.service';
     `]
 })
 
-export class CreateEventComponent implements OnInit {
+export class CreateEventComponent {
     isDirty:Boolean = true
     newEvent
     constructor(private router:Router, private eventService:EventService) { }
-
-    ngOnInit() {
-        this.newEvent = {
-            name: 'Ng Spectacular',
-            date:'8/8/2028',
-            time: '10am',
-            price: 799.99,
-            location: {
-                address: '456 Happy St',
-                city: 'California',
-                country: 'USA'
-            },
-            onlineUrl: 'http://ngSpectacular.com',
-            imageUrl: 'http;//ngSpectacular.com/logo.png'
-        }
-    }
 
     saveEvent(formValues) {
         this.eventService.saveEvent(formValues)
