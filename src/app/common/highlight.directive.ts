83b4393e4bdea35f234bac1051c94a5a94ad2d8f
Achut Cherukuri <Achut.Cherukuri@primeroedge.com>
2019-02-04 16:35:26 -0600

    used HostBindings and HostListeners instead of Event Listeners in Directives

--- src/app/common/highlight.directive.ts
+++ src/app/common/highlight.directive.ts
@@ -1,32 +1,29 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Input, HostListener, HostBinding, SecurityContext } from "@angular/core";
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { NgStyle } from '@angular/common';
 
 @Directive({
     selector: '[highlight]'
 })
 
-export class HighlightDirective implements OnInit {
+export class HighlightDirective {
     private element: HTMLInputElement;
+    @Input('highlight') color: string;
+    // @Input() highlight:string 
     
-    constructor(private elRef: ElementRef){
-        this.element =  elRef.nativeElement
+    @HostBinding('style.backgroundColor') private bgColor: string;
+    @HostBinding('style.boxShadow') private boxShadow: SafeStyle;
+
+    @HostListener('mouseenter') onMouseEnter(){
+        this.boxShadow = this.santizer.bypassSecurityTrustStyle("0 8px 10px 1px rgba(0,0,0,0.14), 0 3px 14px 2px rgba(0,0,0,0.12), 0 5px 5px -3px rgba(0,0,0,0.20)")
+        this.bgColor = this.color
     }
 
-    ngOnInit(){
-        this.element.addEventListener('mouseover',e =>{
-            this.element.style.boxShadow = 
-                ` 0 8px 10px 1px rgba(0,0,0,0.14), 
-                  0 3px 14px 2px rgba(0,0,0,0.12), 
-                  0 5px 5px -3px rgba(0,0,0,0.20)
-                `
-            this.element.style.backgroundColor = 'yellow'
-        })
-        this.element.addEventListener('mouseout', e =>{
-            this.element.style.boxShadow = 
-                ` 0 2px 1px 1px rgba(0,0,0,0.2),
-                  0 1px 1px 0 rgba(0,0,0,0.14),
-                  0 1px 3px 0 rgba(0,0,0,0.12)
-                `
-            this.element.style.backgroundColor = "white"
-        })
+    @HostListener('mouseleave') onMouseLeave() {
+        
+        this.boxShadow = this.santizer.bypassSecurityTrustStyle(` 0 2px 1px 1px rgba(0,0,0,0.2), 0 1px 1px 0 rgba(0,0,0,0.14), 0 1px 3px 0 rgba(0,0,0,0.12)`)
+        this.bgColor = 'white'
     }
+    constructor(private santizer: DomSanitizer) { }
+
 }
