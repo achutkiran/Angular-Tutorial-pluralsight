1d32380ac561a9363b8e973edae165190e047cd3
Achut Cherukuri <Achut.Cherukuri@primeroedge.com>
2019-02-07 09:54:15 -0600

    added cors

--- server/expressConfig.ts
+++ server/expressConfig.ts
@@ -5,10 +5,24 @@ import * as session from 'express-session'
 import * as passport from 'passport';
 import * as express from 'express';
 import * as path from 'path';
+import * as cors from 'cors';
+const whitelist = ['http://localhost:4200']
+
+const corsOptions:cors.CorsOptions = {
+    origin: function (origin, callback) {
+        if(whitelist.indexOf(origin) !== -1){
+            callback(null, true)
+        }
+        else {
+            callback(null, false)
+        }
+    }
+}
 
 let rootPath = path.normalize(__dirname + '/../../');
 
 export default function(app:Express){
+    app.use(cors(corsOptions))
     app.use(cookieParser());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:false}));
